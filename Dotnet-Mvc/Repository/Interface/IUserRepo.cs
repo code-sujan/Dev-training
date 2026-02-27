e1a36d07f57df4ab66234a5d4b78967cb43e817f
shikhabista <shikhabista33@gmail.com>
2026-02-27 12:14:51 +0545

    chore:user repo created

---
+++ Dotnet-Mvc/Repository/Interface/IUserRepo.cs
@@ -0,0 +1,8 @@
+﻿using Dotnet_Mvc.Entities;
+
+namespace Dotnet_Mvc.Repository.Interface;
+
+public interface IUserRepo
+{
+    void Create(User user);
+}
---
+++ Dotnet-Mvc/Repository/UserRepo.cs
@@ -0,0 +1,14 @@
+﻿using Dotnet_Mvc.Entities;
+using Dotnet_Mvc.Repository.Interface;
+using Microsoft.EntityFrameworkCore;
+
+namespace Dotnet_Mvc.Repository;
+
+public class UserRepo(DbContext context) : IUserRepo
+{
+    public void Create(User user) => context.Set<User>().Add(user);
+    public void Update(User user) => context.Set<User>().Update(user);
+    public void Remove(User user) => context.Set<User>().Remove(user);
+    public IQueryable GetQueryable() => context.Set<User>();
+    public void Commit() => context.SaveChanges();
+}
